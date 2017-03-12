6aaaaf1cd3735d8314f452eecf6dbb994ad7b56e
Doychin Atanasov <ironsmile@gmail.com>
2017-03-12 02:31:29 +0200

    Simplify Ray and remove unused cameras

--- geometry/ray.go
+++ geometry/ray.go
@@ -4,32 +4,9 @@ type Ray struct {
 	Origin    Point
 	Direction Vector
 
-	Mint float64
-	Maxt float64
-
-	Time float64
-
-	Depth int
-
 	Debug bool
 }
 
-func (r *Ray) AtTime(time float64) *Point {
-	return r.Origin.PlusVector(r.Direction.MultiplyScalar(time))
-}
-
 func (r *Ray) BackToDefaults() {
-	r.Mint = 0.0
-	r.Maxt = 0.0
-	r.Time = 0.0
-	r.Depth = 0
 	r.Debug = false
-}
-
-func NewRay(origin *Point, dir *Vector) *Ray {
-	return &Ray{Origin: *origin, Direction: *dir.NormalizeIP(), Debug: false}
-}
-
-func NewRayFull(origin *Point, dir *Vector, start, end float64) *Ray {
-	return &Ray{Origin: *origin, Direction: *dir, Mint: start, Maxt: end}
 }
