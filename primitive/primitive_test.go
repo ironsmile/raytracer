522ddd65f0adbc10798531741aeb5d2cd70a7a42
Doychin Atanasov <ironsmile@gmail.com>
2017-03-16 21:47:24 +0200

    Create a test for intersecting a rectangle

--- primitive/primitive_test.go
+++ primitive/primitive_test.go
@@ -1,9 +1,12 @@
 package primitive
 
 import (
+	"math"
 	"testing"
 
 	"github.com/ironsmile/raytracer/geometry"
+	"github.com/ironsmile/raytracer/shape"
+	"github.com/ironsmile/raytracer/transform"
 )
 
 func BenchmarkPrimitiveIntersection(t *testing.B) {
@@ -15,5 +18,32 @@ func BenchmarkPrimitiveIntersection(t *testing.B) {
 
 	for i := 0; i < t.N; i++ {
 		prim.Intersect(ray, 1000000.0)
+	}
+}
+
+func TestRectangleReturnedDistanceToIntersection(t *testing.T) {
+	rect := NewRectangle(1, 1)
+	rect.SetTransform(transform.Translate(geometry.NewVector(0, 0, 30)))
+	ray := geometry.Ray{
+		Origin:    geometry.Point{X: 0, Y: 0, Z: 0},
+		Direction: geometry.Vector{X: 0, Y: 0, Z: 1},
+	}
+
+	hitMiss, distance, normal := rect.Intersect(ray, 100)
+
+	if hitMiss != shape.HIT {
+		t.Error("The rectangle.Intersect method failed: false negative")
+	}
+
+	expectedNormal := geometry.NewVector(0, 0, -1)
+
+	if !normal.Equals(expectedNormal) {
+		t.Errorf("Wrong normal returned by rectangle.Intersect. Expected %s but got %s",
+			expectedNormal, normal)
+	}
+
+	if math.Abs(distance-30) > 0.001 {
+		t.Errorf("Wrong distance returned by rectangle.Intersect. Exlected %f but got %f",
+			30.0, distance)
 	}
 }
