9594c561e145e22eb87be3138b9afa61e4d106af
Doychin Atanasov <ironsmile@gmail.com>
2017-03-16 21:56:21 +0200

    Add more intersection benchmarks for different shapes

--- primitive/primitive_test.go
+++ primitive/primitive_test.go
@@ -10,15 +10,36 @@ import (
 )
 
 func BenchmarkPrimitiveIntersection(t *testing.B) {
-	prim := NewSphere(geometry.Point{X: 0, Y: 0, Z: 0}, 2)
 	ray := geometry.Ray{
 		Origin:    geometry.Point{X: 0, Y: 0, Z: -3},
 		Direction: geometry.Vector{X: 0, Y: 0, Z: 1},
 	}
 
-	for i := 0; i < t.N; i++ {
-		prim.Intersect(ray, 1000000.0)
-	}
+	t.Run("Sphere", func(t *testing.B) {
+		shpere := NewSphere(geometry.Point{X: 0, Y: 0, Z: 0}, 2)
+		for i := 0; i < t.N; i++ {
+			shpere.Intersect(ray, 1000000.0)
+		}
+	})
+
+	t.Run("Triangle", func(t *testing.B) {
+		triangle := NewTriangle([3]geometry.Point{
+			*geometry.NewPoint(-1, -1, 0), // a
+			*geometry.NewPoint(0, 1, -3),  // b
+			*geometry.NewPoint(1, -1, 3),  // c
+		})
+		for i := 0; i < t.N; i++ {
+			triangle.Intersect(ray, 1000000.0)
+		}
+	})
+
+	t.Run("Rectangle", func(t *testing.B) {
+		rect := NewRectangle(1, 1)
+		rect.SetTransform(transform.Translate(geometry.NewVector(0, 0, 30)))
+		for i := 0; i < t.N; i++ {
+			rect.Intersect(ray, 1000000.0)
+		}
+	})
 }
 
 func TestRectangleReturnedDistanceToIntersection(t *testing.T) {
