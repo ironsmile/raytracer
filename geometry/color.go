57e630a78228db815db362dd163e7fe1aa614137
Doychin Atanasov <ironsmile@gmail.com>
2017-04-09 12:20:58 +0300

    Make sure color operations do not overflow 0-1 color range

--- geometry/color.go
+++ geometry/color.go
@@ -1,9 +1,19 @@
 package geometry
 
+import (
+	"github.com/ironsmile/raytracer/utils"
+)
+
 type Color struct {
 	red   float64
 	green float64
 	blue  float64
+}
+
+func (c *Color) clamp() {
+	c.red = utils.Clamp(c.red, 0, 1)
+	c.green = utils.Clamp(c.green, 0, 1)
+	c.blue = utils.Clamp(c.blue, 0, 1)
 }
 
 func (c *Color) Red() float64 {
@@ -23,11 +33,14 @@ func (c *Color) RGBA() (r, g, b, a uint32) {
 }
 
 func (c *Color) Plus(other *Color) *Color {
-	return &Color{c.red + other.red, c.green + other.green, c.blue + other.blue}
+	r := &Color{c.red + other.red, c.green + other.green, c.blue + other.blue}
+	r.clamp()
+	return r
 }
 
 func (c *Color) PlusIP(other *Color) *Color {
 	c.red, c.green, c.blue = c.red+other.red, c.green+other.green, c.blue+other.blue
+	c.clamp()
 	return c
 }
 
@@ -41,16 +54,20 @@ func (c *Color) MultiplyIP(other *Color) *Color {
 }
 
 func (c *Color) MultiplyScalar(sclr float64) *Color {
-	return &Color{c.red * sclr, c.green * sclr, c.blue * sclr}
+	r := &Color{c.red * sclr, c.green * sclr, c.blue * sclr}
+	r.clamp()
+	return r
 }
 
 func (c *Color) MultiplyScalarIP(sclr float64) *Color {
 	c.red, c.green, c.blue = c.red*sclr, c.green*sclr, c.blue*sclr
+	c.clamp()
 	return c
 }
 
 func (c *Color) Set(red, green, blue float64) {
 	c.red, c.green, c.blue = red, green, blue
+	c.clamp()
 }
 
 func NewColor(r, g, b float64) *Color {
