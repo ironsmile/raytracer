4b0c0267eb57c2e4260020f756d40e67c086616f
Doychin Atanasov <ironsmile@gmail.com>
2017-04-19 19:40:10 +0300

    Add a ray to ray intersection method

--- geometry/ray.go
+++ geometry/ray.go
@@ -1,8 +1,6 @@
 package geometry
 
-import (
-	"math"
-)
+import "math"
 
 // Ray represents a straight line with origin and a direction
 type Ray struct {
@@ -25,6 +23,37 @@ func (r *Ray) BackToDefaults() {
 // At returns the point which as at distance t from the Origin in Direction
 func (r *Ray) At(t float64) Vector {
 	return r.Origin.Plus(r.Direction.MultiplyScalar(t))
+}
+
+// Intersect returns the intersection point between two rays. Two rays may not always
+// intersect so that the second argument says wether there is an intersectoin at all
+func (r *Ray) Intersect(o Ray) (Vector, bool) {
+	n := r.Direction.Y*r.Origin.Z + r.Direction.Z*o.Origin.Y - r.Direction.Z*r.Origin.Y -
+		r.Direction.Y*o.Origin.Z
+
+	m := r.Direction.Y*o.Direction.Z - r.Direction.Z*o.Direction.Y
+
+	v := n / m
+
+	if v < 0 && v > -EPSILON {
+		v = 0
+	}
+
+	if v < 0 || v > o.Maxt {
+		return o.Direction, false
+	}
+
+	u := (o.Origin.Y + o.Direction.Y*v - r.Origin.Y) / r.Direction.Y
+
+	if u < 0 && u > -EPSILON {
+		u = 0
+	}
+
+	if u < 0 || u > r.Maxt {
+		return o.Direction, false
+	}
+
+	return r.At(u), true
 }
 
 // NewRay retursn a new ray with Min zero and Max the maximum float64 value
