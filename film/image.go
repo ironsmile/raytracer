44ce51d415dbc712aee0e1734ba99b4acb76bbeb
Doychin Atanasov <ironsmile@gmail.com>
2017-06-18 22:32:25 +0300

    Use weighted color buffer in the PNG image output as well

--- film/image.go
+++ film/image.go
@@ -5,14 +5,17 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"os"
+
+	rColor "github.com/ironsmile/raytracer/color"
 )
 
 type Image struct {
 	width  int
 	height int
 
-	img *image.NRGBA
+	buf rColor.Buf
 
 	filename string
 }
@@ -24,9 +27,7 @@ func (i *Image) Wait() {
 func (i *Image) Init(width int, height int) error {
 	i.width = width
 	i.height = height
-
-	i.img = image.NewNRGBA(image.Rect(0, 0, width, height))
-
+	i.buf = rColor.NewBuf(width * height)
 	return nil
 }
 
@@ -49,21 +50,35 @@ func (i *Image) DoneFrame() {
 		out.Close()
 	}()
 
-	err = png.Encode(out, i.img)
+	img := image.NewNRGBA(image.Rect(0, 0, i.width, i.height))
+	buffSize := i.width * i.height
+	for ind := 0; ind < buffSize; ind++ {
+		y := ind / i.width
+		x := ind % i.width
+		bufClr := i.buf.Get(ind)
+		img.Set(x, y, color.NRGBA64{
+			R: uint16(bufClr.Red() * float64(math.MaxUint16)),
+			G: uint16(bufClr.Green() * float64(math.MaxUint16)),
+			B: uint16(bufClr.Blue() * float64(math.MaxUint16)),
+			A: math.MaxUint16,
+		})
+	}
+
+	err = png.Encode(out, img)
 
 	if err != nil {
-		fmt.Errorf("%s\n", err.Error())
+		fmt.Printf("encoding image failed: %s\n", err)
 	} else {
 		fmt.Printf("Image saved to %s\n", i.filename)
 	}
 }
 
-func (i *Image) StartFrame() {
-
-}
+func (i *Image) StartFrame() {}
 
 func (i *Image) Set(x, y int, clr color.Color) error {
-	i.img.Set(x, y, clr)
+	ind := i.width*y + x
+	ri, gi, bi, _ := clr.RGBA()
+	i.buf.UpdateRGB(ind, float32(ri)/65535.0, float32(gi)/65535.0, float32(bi)/65535.0)
 	return nil
 }
 
