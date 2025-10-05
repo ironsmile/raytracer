0163808393538994ab00401daaf2c01abd4eeeb5
Doychin Atanasov <ironsmile@gmail.com>
2025-10-05 13:54:48 +0300

    Calculate the random positions within a sample ahead of time

--- sampler/sub.go
+++ sampler/sub.go
@@ -12,17 +12,16 @@ var ErrSubSamplerEnd = errors.New("End of sub sampler")
 // SubSampler generates samples for a rectangular subsection of a sampler
 type SubSampler struct {
 	pixArray []sampledPixel
+	randoms  []sampleRand
 
 	current     uint32
 	perPixel    uint32
 	samplesDone uint32
 
 	parent *SimpleSampler
-
-	rand *rand.Rand
 }
 
-// GetSample returns a single sample which should be raytraced
+// GetSample returns a single sample which should be raytraced.
 func (s *SubSampler) GetSample() (x, y float64, err error) {
 	if s.current >= uint32(len(s.pixArray)) {
 		if s.samplesDone+1 >= s.perPixel {
@@ -38,8 +37,8 @@ func (s *SubSampler) GetSample() (x, y float64, err error) {
 	}
 	sample := s.pixArray[s.current]
 
-	x = float64(sample.x) + s.rand.Float64()
-	y = float64(sample.y) + s.rand.Float64()
+	x = float64(sample.x) + s.randoms[s.samplesDone].x
+	y = float64(sample.y) + s.randoms[s.samplesDone].y
 	s.current++
 	return
 }
@@ -50,15 +49,30 @@ func (s *SubSampler) Reset() {
 	s.current = 0
 }
 
-// NewSubSampler returns a sumb sampler.... TODO
+// NewSubSampler returns a sub sampler which is responsible for a particular set of
+// pixels on the screen.
 func NewSubSampler(pixArray []sampledPixel, perPixel uint32, p *SimpleSampler) *SubSampler {
 	src := rand.NewSource(time.Now().UnixMicro())
 	rnd := rand.New(src)
+
+	rands := make([]sampleRand, 0, perPixel)
+	for range perPixel {
+		rands = append(rands, sampleRand{
+			x: rnd.Float64(),
+			y: rnd.Float64(),
+		})
+	}
 
 	return &SubSampler{
 		pixArray: pixArray,
 		perPixel: perPixel,
 		parent:   p,
-		rand:     rnd,
+		randoms:  rands,
 	}
+}
+
+// sampleRand is a random position within a sampled pixel.
+type sampleRand struct {
+	x float64
+	y float64
 }
